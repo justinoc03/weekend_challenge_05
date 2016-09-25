6c0f3abe6a357bb7a931ea2892808474dbc0a448
Justin O'Connor <justin.oc03@gmail.com>
2016-09-24 19:26:09 -0500

    initial commit with angular sourced

---
+++ server/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set("port", (process.env.PORT || 1000));
+
+app.get("/*", function(req, res){
+  console.log("Here is the property:", req.params[0]);
+  var file = req.params[0] || '/views/index.html';
+  res.sendFile(path.join(__dirname, "/public/", file));
+});
+
+app.listen(app.get("port"), function(){
+  console.log("FTL drive spun up on:", app.get("port") + ". Brace for jump.");
+});
---
+++ server/public/scripts/client.js
@@ -0,0 +1,20 @@
+var myApp = angular.module("myApp", ["ngRoute"]);
+
+myApp.config("$routeProvider", function($routeProvider){
+  $routeProvider.
+    when("/home", {
+      templateUrl: "/views/partials/home.html",
+      controller: "homeController"
+    }).
+    when("/addPet", {
+      templateUrl: "/views/partials/addPet.html",
+      controller: "addPetController"
+    }).
+    when("/viewPets", {
+      templateUrl: "/views/partials/viewPets.html",
+      controller: "viewPetsController"
+    }).
+    otherwise({
+      redirectTo: "/home"
+    });
+});
